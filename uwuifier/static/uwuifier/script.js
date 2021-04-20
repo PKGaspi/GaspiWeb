6994927c0e377fca7fbe342433ba42c76549f892
Juan Gaspar García <gaxdpar@gmail.com>
2021-04-20 17:24:44 +0300

    Improved uwuifier
    Added more face varieties and phrases to copy button

--- uwuifier/static/uwuifier/script.js
+++ uwuifier/static/uwuifier/script.js
@@ -1,17 +1,41 @@
 var texts = [
     "copy succesfuw!",
-    "doubwe coppy!!",
-    "twipwe coppy!!!",
+    "doubwe copy!!",
+    "twipwe copy!!!",
 ]
+
+var happy_faces = [
+    "(ノ*゜▽゜*)",
+    "( ﾟ▽ﾟ)/",
+    "(^・ω・^ )",
+    "(*´꒳`*)",
+    "(^-^*)/",
+    "(⑅˘͈ ᵕ ˘͈ )",
+    "◟(๑•͈ᴗ•͈)◞",
+]
+
 var more_texts = [
     "cwiticaw copy!!!!",
     "godwike copy!!!!",
     "supewefective copy!!!!",
     "wegendawy copy!!!!",
+    "bestest copy!!!!",
+    "copycopycopycopy!!!!"
+]
+
+var angry_faces = [
+    "ヽ(♯｀Д´)ﾉ",
+    "(/ﾟДﾟ)/",
+    " ヾ(｀ε´)ﾉ",
+    "（｀Δ´）！",
+    "(๑･`▱´･๑)",
+    "٩(๑`ȏ´๑)۶",
+    "ヾ(*｀⌒´*)ﾉ",
 ]
 
 var n_copies = 0
 var text = "";
+var face = "";
 
 function copyToClipboard() {
     /* Get the text field */
@@ -32,21 +56,27 @@ function copyToClipboard() {
     /* Remove artificial textarea */
     document.body.removeChild(copyText);
     
-    /* Alert the copied text */
+    /* Change button text */
     const copyBtn = document.getElementById("copy-btn")
     if (n_copies < texts.length){
 	text = texts[n_copies];
+	var old_face = face;
+	while (face.localeCompare(old_face) == 0) {
+	    face = happy_faces[Math.floor(Math.random() * happy_faces.length)];
+	}
 	n_copies++;
-	copyBtn.className = "btn btn-success"
+	copyBtn.className = "btn btn-success";
     }
     else {
-	var new_text = text;
-	while (text.localeCompare(new_text) == 0) {
-	    new_text = more_texts[Math.floor(Math.random() * more_texts.length)];
+	var old_text = text;
+	while (text.localeCompare(old_text) == 0) {
+	    text = more_texts[Math.floor(Math.random() * more_texts.length)];
 	}
-	text = new_text;
+	var old_face = face;
+	while (face.localeCompare(old_face) == 0) {
+	    face = angry_faces[Math.floor(Math.random() * angry_faces.length)];
+	}
 	copyBtn.className = "btn btn-danger"
     }
-    copyBtn.innerHTML = text + " " + document.getElementById("smiley").innerHTML;
-    //alert("Copied the text: " + copyText.value);
+    copyBtn.innerHTML = text + " " + face;
 }
